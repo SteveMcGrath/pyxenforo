0b3f13aab1b7e363c25ab1606ec6b32ca19978b4
Steven McGrath <steve@chigeek.com>
2011-06-03 23:47:47 -0500

    Created basic setup.py script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='XenForo', 
+      version='0.1',
+      description='Python XenForo api',
+      author='Steven McGrath',
+      author_email='steve@chigeek.com',
+      url='http://chigeek.com',
+      py_modules=['xenforo'],
+     )
